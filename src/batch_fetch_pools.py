423c8dc07af1b778eece09931340f810306d1a92
Harshit Shah <harshit@Harshits-MacBook-Air.local>
2025-07-02 15:18:39 +0530

    fetched pools for 1st & 2nd jul25

--- src/batch_fetch_pools.py
+++ src/batch_fetch_pools.py
@@ -37,9 +37,9 @@ def run_batch(start_day, end_day, month, year, save_subdir):
 if __name__ == "__main__":
     # Edit this section to change the batch month/year
     run_batch(
-        start_day=29,
-        end_day=31,
-        month=6,
+        start_day=1,
+        end_day=2,
+        month=7,
         year=2025,
-        save_subdir="jun25"
+        save_subdir="jul25"
     )
